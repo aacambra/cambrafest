869f91f17a8bcabc999d999ed456cadfbabb37c5
Marcelo Almeida <me@malmeida.dev>
2023-07-28 11:38:21 +0100

    Change header links with redirects and improved sitemap.xml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactRefresh: true,
+  reactRefresh: true,
+  async redirects() {
+    return [
+      {
+        source: '/bilhetes',
+        destination: 'https://musicasemcapa.bol.pt/Comprar/Pesquisa?q=cambrafest',
+        permanent: false,
+      },
+      {
+        source: '/info',
+        destination: 'https://linktr.ee/cambrafest',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
