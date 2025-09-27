0df93276eaa8a7fa4f0fc3c7f09bf7d821c7d305
Eduardo Campos <eduardocampos@MacBook-Pro-de-Eduardo.local>
2025-09-27 17:32:39 +0100

    Updates

--- _next/static/chunks/app/cambrafest/info/page-86bbd3c7495d0f34.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[930],{4908:function(e,r,n){Promise.resolve().then(n.bind(n,7926))},7926:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return f}});var t=n(9268),o=n(6006);function f(){return(0,o.useEffect)(()=>{window.location.href="https://linktr.ee/cambrafest"},[]),(0,t.jsx)("p",{children:"Redirecionando para o link do site..."})}},3177:function(e,r,n){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var t=n(6006),o=Symbol.for("react.element"),f=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),i=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,s={key:!0,ref:!0,__self:!0,__source:!0};function u(e,r,n){var t,u={},c=null,_=null;for(t in void 0!==n&&(c=""+n),void 0!==r.key&&(c=""+r.key),void 0!==r.ref&&(_=r.ref),r)f.call(r,t)&&!s.hasOwnProperty(t)&&(u[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===u[t]&&(u[t]=r[t]);return{$$typeof:o,type:e,key:c,ref:_,props:u,_owner:i.current}}r.jsx=u,r.jsxs=u},9268:function(e,r,n){"use strict";e.exports=n(3177)}},function(e){e.O(0,[253,698,744],function(){return e(e.s=4908)}),_N_E=e.O()}]);
